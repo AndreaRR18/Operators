1592c741a09cecb2518577bb4104d75f13c1bccc
Andrea <andrea.rinaldi91@gmail.com>
2020-01-21 07:43:53 +0100

    Add test to all function in Pipeforward

--- Tests/OperatorsTests/PipeforwardTests.swift
+++ Tests/OperatorsTests/PipeforwardTests.swift
@@ -4,6 +4,7 @@ import SwiftCheck
 
 final class PipeForwardTests: XCTestCase {
     
+    //MARK - Pipeforward
     func testPipeForwardOperators_GetAValueAndAFunction_ReturnNewValueInstance_IntToInt() {
         property("For every int value the pipe forward return an incr of that value") <- forAll { (i : Int) in
             let sut = (i |> incr)
@@ -21,10 +22,58 @@ final class PipeForwardTests: XCTestCase {
     }
     
     func testPipeForwardOperators_GetAValueAndAFunction_ReturnNewValueInstance_ArrayIntToArrayString() {
-           property("For every array of int value we can get the matching array of string") <- forAll { (i : [Int]) in            
+        property("For every array of int value we can get the matching array of string") <- forAll { (i : [Int]) in
             let sut = (i.map { $0 |> toString })
             let expect = i.map(toString)
             return sut == expect
-           }
-       }
+        }
+    }
+    
+    //MARK - Pipeforward inout
+    func testInoutForwardOperator_GetAValueAndInoutFunction_ReturnMutatedValueInstance_IntToInt() {
+        property("For every int value the pipe function return an incr of that value") <- forAll { (i : Int) in
+            let inoutFunction = toInout(incr)
+            var sut = i
+            sut |> inoutFunction
+            let expect = (i + 1)
+            return sut == expect
+        }
+    }
+    
+    //MARK - Pipe function
+    func testPipeFunction_GetAValueAndAFunction_ReturnNewValueInstance_IntToInt() {
+        property("For every int value the pipe function return an incr of that value") <- forAll { (i : Int) in
+            let sut = with(i, incr)
+            let expect = (i + 1)
+            return sut == expect
+        }
+    }
+    
+    func testPipeFunction_GetAValueAndAFunction_ReturnNewValueInstance_IntToString() {
+        property("For every int value we can get the matching string") <- forAll { (i : Int) in
+            let sut = with(i, toString)
+            let expect = "\(i)"
+            return sut == expect
+        }
+    }
+    
+    func testPipeFunction_GetAValueAndAFunction_ReturnNewValueInstance_ArrayIntToArrayString() {
+        property("For every array of int value we can get the matching array of string") <- forAll { (i : [Int]) in
+            let sut = (i.map { with($0, toString) })
+            let expect = i.map(toString)
+            return sut == expect
+        }
+    }
+    
+    //MARK - Pipe function inout
+    func testInoutPipeInoutFunction_GetAValueAndInoutFunction_ReturnMutatedValueInstance_IntToInt() {
+        property("For every int value the pipe function return an incr of that value") <- forAll { (i : Int) in
+            let inoutFunction = toInout(incr)
+            var sut = i
+            withInout(&sut,  inoutFunction)
+            let expect = (i + 1)
+            return sut == expect
+        }
+    }
+    
 }
